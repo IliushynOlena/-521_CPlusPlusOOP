ff98a418e12f9f7e416ff029f86be437d00573c2
ilusin_o@itstep.academy <ilusin_o@itstep.academy>
2026-01-31 12:05:09 +0200

    Stack

---
+++ 12_Stack/12_Stack.cpp
@@ -0,0 +1,83 @@
+﻿
+#include <iostream>
+using namespace std;
+
+class Stack
+{
+    enum {EMPTY = -1, FULL = 9};
+    int arr[FULL + 1];
+    int topIndex;
+public:
+    Stack()
+    {
+        topIndex = EMPTY;
+    }
+    bool ISFULL()
+    {
+        return topIndex == FULL;
+    }
+    bool IsEmpty()
+    {
+        return topIndex == EMPTY;
+    }
+    bool Push(int elem)
+    {
+        if(!ISFULL())
+        { 
+            topIndex++;
+            arr[topIndex] = elem;
+            return true;
+        }
+        return false;
+    }
+    int Pop()
+    {
+        if (!IsEmpty())
+        {
+            return arr[topIndex--];
+        }
+    }
+    int GetCount()
+    {
+        return topIndex + 1;
+    }
+    void Clear()
+    {
+        topIndex = EMPTY;
+    }
+    int Peek()
+    {
+        if (!IsEmpty())
+        {
+            return arr[topIndex];
+        }
+    }
+    void Print()
+    {
+        for (int i = 0; i <= topIndex; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+};
+
+int main()
+{
+    Stack st;
+    st.Push(1);
+    st.Push(2);
+    st.Push(3);
+    st.Push(4);
+    st.Print();
+    cout << "Last element : " << st.Peek() << endl;
+
+    while (!st.IsEmpty())
+    {
+        cout << "Element : " << st.Pop() << endl;
+    }   
+    cout << "Size : " << st.GetCount() << endl;
+
+}
+
