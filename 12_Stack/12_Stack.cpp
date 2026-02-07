926e1722d57d26fe7eb8edfb3c5a7001ec40384d
IliushynOlena <lenailyshun@gmail.com>
2026-02-07 11:47:19 +0200

    Stack dynamic

--- 12_Stack/12_Stack.cpp
+++ 12_Stack/12_Stack.cpp
@@ -2,55 +2,117 @@
 #include <iostream>
 using namespace std;
 
+//class Stack
+//{
+//    enum {EMPTY = -1, FULL = 9};
+//    int arr[FULL + 1];
+//    int topIndex;
+//public:
+//    Stack()
+//    {
+//        topIndex = EMPTY;
+//    }
+//    bool ISFULL()
+//    {
+//        return topIndex == FULL;
+//    }
+//    bool IsEmpty()
+//    {
+//        return topIndex == EMPTY;
+//    }
+//    bool Push(int elem)
+//    {
+//        if(!ISFULL())
+//        { 
+//            topIndex++;
+//            arr[topIndex] = elem;
+//            return true;
+//        }
+//        return false;
+//    }
+//    int Pop()
+//    {
+//        if (!IsEmpty())
+//        {
+//            return arr[topIndex--];
+//        }
+//    }
+//    int GetCount()
+//    {
+//        return topIndex + 1;
+//    }
+//    void Clear()
+//    {
+//        topIndex = EMPTY;
+//    }
+//    int Peek()
+//    {
+//        if (!IsEmpty())
+//        {
+//            return arr[topIndex];
+//        }
+//    }
+//    void Print()
+//    {
+//        for (int i = 0; i <= topIndex; i++)
+//        {
+//            cout << arr[i] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//};
+
 class Stack
 {
-    enum {EMPTY = -1, FULL = 9};
-    int arr[FULL + 1];
+    enum {EMPTY = -1};
+    int* arr;
+    int size;
     int topIndex;
 public:
-    Stack()
+    //delete  default
+    Stack() = default; //Stack(){}
+    Stack(const Stack &other) = delete; //Stack(){}
+    Stack(int size): size(size)//5
     {
+        arr = new int[size];
         topIndex = EMPTY;
     }
-    bool ISFULL()
+    bool IsFull()const
     {
-        return topIndex == FULL;
+        return topIndex == size - 1;
+    }
+    bool Push(int element)
+    {
+        if (!IsFull())
+        {
+          
+            arr[++topIndex] = element;//++topIndex;  arr[topIndex] = element <----
+            return true;
+        }
+        return false;
     }
     bool IsEmpty()
     {
         return topIndex == EMPTY;
     }
-    bool Push(int elem)
-    {
-        if(!ISFULL())
-        { 
-            topIndex++;
-            arr[topIndex] = elem;
-            return true;
-        }
-        return false;
-    }
     int Pop()
     {
         if (!IsEmpty())
         {
-            return arr[topIndex--];
+            return arr[topIndex--];//---> return arr[topIndex]; topIndex--;          
         }
-    }
-    int GetCount()
-    {
-        return topIndex + 1;
-    }
-    void Clear()
-    {
-        topIndex = EMPTY;
     }
     int Peek()
     {
         if (!IsEmpty())
         {
-            return arr[topIndex];
+            return arr[topIndex];        
         }
+    }
+    int GetCount()
+    {
+        return topIndex + 1;
     }
     void Print()
     {
@@ -61,15 +123,23 @@ public:
         cout << endl;
     }
 
+
+    ~Stack()
+    {
+        delete[]arr;
+    }
 };
+
 
 int main()
 {
-    Stack st;
-    st.Push(1);
-    st.Push(2);
-    st.Push(3);
-    st.Push(4);
+    Stack st(10);
+
+
+    st.Push(11);
+    st.Push(20);
+    st.Push(31);
+    st.Push(14);
     st.Print();
     cout << "Last element : " << st.Peek() << endl;
 
---
+++ 13_Queue/13_Queue.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class Queue
+{
+    enum {EMPTY = 0};
+    int* arr;
+    int maxSize;
+    int topIndex;
+public:
+    Queue() = default;
+    Queue(int size) : maxSize(size)
+    {
+        arr = new int[maxSize];
+        topIndex = EMPTY;
+    }
+    bool IsEmpty()
+    {
+        return topIndex == EMPTY;
+    }
+    bool IsFull()
+    {
+        return topIndex == maxSize - 1;
+    }
+    void Enqueue(int element)
+    {
+        if (!IsFull())
+            arr[topIndex++] = element;//arr[topIndex] = element;  topIndex++;      
+    }
+  /*  int Dequeue()
+    {
+    
+        if (!IsEmpty())
+        {
+            int first = arr[0];
+            for (int i = 0; i < topIndex; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            topIndex--;
+            return first;
+        }
+    }*/
+    int Dequeue()
+    {
+        // cycle queue
+        if (!IsEmpty())
+        {
+            int first = arr[0];
+            for (int i = 0; i < topIndex; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+            //topIndex--;
+            arr[topIndex-1] = first;
+            return first;
+        }
+    }
+    int GetCount()
+    {
+        return topIndex;
+    }
+    int Clear()
+    {
+        topIndex = 0;
+    }
+    void Print()
+    {
+        for (int i = 0; i < topIndex; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+    ~Queue()
+    {
+        if (arr != nullptr)delete[]arr;
+    }
+};
+
+int main()
+{
+    Queue q(15);
+    for (int i = 0; i < 10; i++)
+    {
+        q.Enqueue(rand() % 25);
+    }
+    q.Print();
+
+    while (!q.IsEmpty())
+    {
+        cout << q.Dequeue() << endl;
+        q.Print();
+        _getch();
+    }
+    
+}
+
